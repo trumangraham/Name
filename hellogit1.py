9519667d83ec8c6c3bc924805422bdf6a5e71395
trumangraham <trumangraham@amdg.rockhursths.edu>
2020-02-07 08:49:24 -0600

    initial

---
+++ hellogit1.py
@@ -0,0 +1,66 @@
+# comment 
+print("Hello, World!")
+a = 6 # variables
+b = 5
+print (a + b)
+
+# how to make a list
+	
+myList = [1, 2, 3, 4, 3, 2, 1]
+
+#how to stride
+print (
+
+#strings are just lists
+phrase = 'Catch the dog'
+phrase
+
+#how to acsess elements
+
+print(myList[1])
+
+while(x<5)
+    x=0
+    x+=1
+
+#how to write a function
+def sumFunction(a, b):
+	return a + b
+
+#how to call a function
+
+print(sumFunction(2,20))
+
+x = 10
+y = -10
+
+
+#blocks of code
+if(x == 10):	
+	x=5
+elif(y == -10):
+	y=5
+else:
+	x = y - x
+
+#loop
+mylist = [1, 3, 8, 412, 43, 2, 20]
+
+
+#length of list
+len(mylist)
+
+#for loop by index
+for i in range(len(mylist)):
+	print(mylist[i])
+
+#for each loop 
+for val in mylist:
+	print(val)
+
+#dictionaries
+lookup = {}
+lookup ['kc'] = 'chiefs'
+lookup ['ne'] = 'patriots'
+lookup ['la'] = 'chargers'
+lookup ['suck'] = 'raiders'
---
+++ randomstuff.py
@@ -0,0 +1,10 @@
+import random
+
+roll = random.randint(1,6)
+
+i=0
+while(i<10):
+    roll = random.randint(1,6)
+    print(roll)
+    i+= 1
+    
