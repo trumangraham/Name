528b76314a60d9aa1c1e27b2b9dd28c89683c8bf
trumangraham <trumangraham@amdg.rockhursths.edu>
2020-02-10 08:29:29 -0600

    gamer

---
+++ myio.py
@@ -0,0 +1,13 @@
+phrase = input("Enter a string: ")
+print("You said " + phrase)
+print(f"You said {phrase}")
+
+someFloat = float(input("Enter a float: "))
+print("You entered float: " + str(someFloat))
+print(f"You entered float: {someFloat}")
+
+someint = float(input("Enter a int: "))
+print("You entered int: " + str(someInt))
+print(f"You entered int: {someInt}")
+
+print(f"Do Python inline, like this: {someFloat} * {someInt} = {someFloat * someInt}")
